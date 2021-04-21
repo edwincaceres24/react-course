0f21db4cd369f29c1218dcb6ff8994db84ba1231
edwincaceres24 <edwin.caceres@pucp.pe>
2021-04-20 22:12:19 -0500

    Class 10

---
+++ src/components/badge.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Badge extends React.Component{  //Creamos la clase extendemos la propieadad de ReactComponent
+    render(){
+        return(
+            <section>
+                <figure>
+                        <img src="" alt=""/>
+                </figure>
+                <h1>Badge</h1>
+            </section>
+        )
+    }
+
+}
+
+
+export default Badge;
--- src/index.js
+++ src/index.js
@@ -7,17 +7,29 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Badge from './components/badge';
 
 let myName= {
     name:'Edwin',
     lastname: 'Caceres',
     age: 30,
+    portfolio: 'https://edwin-caceres.com/pages/portafolio',
 };
-let  { name:mainVar ,lastname, age}=myName; //Desestructuramos el objeto para acceder facil y la asignamos a otra variable
+let  { name:mainVar ,lastname, age , portfolio}=myName; //Desestructuramos el objeto para acceder facil y la asignamos a otra variable
 
-const element = <h1>Hello I'm <i>{mainVar}</i>, this is my first project in React!</h1>;
+const portfolioText = React.createElement('a',{href:portfolio, target: '-blank'}, `portfolio`);
+
+const element = React.createElement('h1',{},`Hello I'm ${mainVar} ${lastname}, I'm ${age} years old and here you can see my `);
+const jsx = 
+(<h1>Hi, my name is {mainVar} {lastname}.
+This is my <a href={portfolio} target='_blank'>portfolio</a> </h1>);
 
 const container = document.getElementById('app');
 
+// let allText = element ;
+
+
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+ReactDOM.render(<Badge />, container);
+
+console.log(portfolioText)
